f6b9bd27df4cc5442f3a8dc63f7f73c3d62c37da
SilviaDumitrache <silvia_d97@yahoo.com>
2020-06-12 20:48:43 +0300

    validators for register page

--- hospital_app/src/app/pages/register/register.page.ts
+++ hospital_app/src/app/pages/register/register.page.ts
@@ -3,6 +3,7 @@ import { AuthenticationService, TokenPayload } from 'src/app/service/authenticat
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ToastController } from '@ionic/angular';
+import { UsernameValidator } from 'src/app/validators/username.validator';
 
 @Component({
   selector: 'app-register',
@@ -31,29 +32,71 @@ export class RegisterPage implements OnInit {
 
   ngOnInit() {
     this.regForm = this.formBuilder.group ({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-      contact: ['', [Validators.required]]
+      // name: ['', [Validators.required]],
+      name: new FormControl('', Validators.compose([
+        // Validators.maxLength(25),
+        Validators.required
+      ])),
+
+      // email: ['', [Validators.required]],
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+	    	Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),  
+
+      // username: ['', [Validators.required]],
+      username: new FormControl('', Validators.compose([
+        // UsernameValidator.validUsername,
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        // Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+        Validators.required
+      ])),
+
+      // password: ['', [Validators.required]],
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(10),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$') //litere(upercase si lowercase)+cifre
+
+      ])),
+
+      // contact: ['', [Validators.required]]
+      contact: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10)
+
+      ]))
     });
   }
 
   validation_mess={
     'name': [
-      { type: 'required', message: 'Introduceti numele!'}
+      { type: 'required', message: 'Introduceti numele.'},
+      // { type: 'maxlength', message: 'Numele nu trebuie sa fie mai lung de 25 de caractere.' },
     ],
     'email': [
-      { type: 'required', message: 'Introduceti email!'}
+      { type: 'required', message: 'Introduceti email.'},
+      { type: 'pattern', message: 'Adresa de email nu este valida' }
     ],
     'username': [
-      { type: 'required', message: 'Introduceti username!'}
+      { type: 'required', message: 'Introduceti username.' },
+      { type: 'minlength', message: 'Introduceti cel putin 5 caractere.' },
+      { type: 'maxlength', message: 'Introduceti cel mullt 25 de caractere.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
     ],
     'password': [
-      { type: 'required', message: 'Introduceti parola!'}
+      { type: 'required', message: 'Introduceti parola.'},
+      { type: 'minlength', message: 'Introduceti cel putin 5 caractere.' },
+      { type: 'maxlength', message: 'Introduceti cel mult 25 de caractere.' },
+      { type: 'pattern', message: 'Introduceti o combinatie de litere si cifre' }
     ],
     'contact': [
-      { type: 'required', message: 'Introduceti nr. de telefon!'}
+      { type: 'required', message: 'Introduceti nr. de telefon!'},
+      { type: 'minlength', message: 'Numarul trebuie sa contina 10 cifre' },
+      { type: 'maxlength', message: 'Numarul trebuie sa contina 10 cifre' }
     ]
   }
 
